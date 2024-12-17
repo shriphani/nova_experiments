719623da26cbad8fbe74bae4a939a36a90c79869
Shriphani Palakodety <spalakod@onai.com>
2024-12-17 09:16:45 -0800

    non-working poseidon example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use ff::{PrimeField, PrimeFieldBits};
+use neptune::{circuit::poseidon_hash, poseidon::PoseidonConstants};
+use nova_snark::{frontend::ConstraintSystem, traits::circuit::StepCircuit};
+
+#[derive(Debug, Clone)]
+pub struct PreImageCircuit<Scalar: PrimeField> {
+    pub pre_image: Vec<Scalar>,
+}
+
+impl<Scalar: PrimeField + PrimeFieldBits> StepCircuit<Scalar> for PreImageCircuit<Scalar> {
+    fn arity(&self) -> usize {
+        1
+    }
+
+    fn synthesize<CS: nova_snark::frontend::ConstraintSystem<Scalar>>(
+        &self,
+        cs: &mut CS,
+        z: &[nova_snark::frontend::num::AllocatedNum<Scalar>],
+    ) -> Result<
+        Vec<nova_snark::frontend::num::AllocatedNum<Scalar>>,
+        nova_snark::frontend::SynthesisError,
+    > {
+        let constants = PoseidonConstants::new_with_strength(neptune::Strength::Standard);
+
+        poseidon_hash(&mut cs, self.pre_image, &constants);
+    }
 }
